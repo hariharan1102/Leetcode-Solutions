0f5ee3d3dcabf61675499db65f7545dfca00ed2a
hariharan1102 <hari89478304@gmail.com>
2026-08-12 09:01:47 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,9 @@
+// Last updated: 8/12/2026, 9:01:46 AM
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+            if(nums[i] == nums[i+1]) return nums[i];
+        return -1;
+    }
+}
