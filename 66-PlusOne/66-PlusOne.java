8756e4e5c8db2e51eb11092beaf04f29309e65d1
hariharan1102 <hari89478304@gmail.com>
2026-08-12 09:03:08 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,15 @@
+// Last updated: 8/12/2026, 9:03:08 AM
+class Solution {
+    public int[] plusOne(int[] digits) {
+      for(int i=digits.length-1;i>=0;i--){
+        if(digits[i]+1!=10){
+            digits[i]+=1;
+            return digits;
+        }
+        digits[i]=0;
+      }  
+      int[] newDigits=new int[digits.length+1];
+      newDigits[0]=1;
+      return newDigits;
+    }
+}
